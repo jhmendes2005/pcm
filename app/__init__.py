c85a7a083d3f2eaf858e7412498c6fa977c5c89d
Joao Henrique <jhmendes2005@gmail.com>
2024-09-28 13:12:14 -0300

    Configure database-engine

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,27 @@
 from flask import Flask, json
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from sqlalchemy import create_engine
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    
+
     # Configurações
-    app.config['SECRET_KEY'] = 'f8ee8897dc09a283101bac9111785cbf1ca69a812a79e23075db7e3c4ecfbcaf'  # Defina uma chave secreta
+    app.config['SECRET_KEY'] = 'f8ee8897dc09a283101bac9111785cbf1ca69a812a79e23075db7e3c4ecfbcaf'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://pcm_leads:Pcm%40123INFRA@pcm_leads.mysql.dbaas.com.br/pcm_leads'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
+    # Configuração do pool de conexões para SQLAlchemy
+    engine = create_engine(
+        app.config['SQLALCHEMY_DATABASE_URI'],
+        pool_pre_ping=True,  # Testa a conexão antes de usá-la
+        pool_size=10,  # Tamanho do pool de conexões
+        max_overflow=20  # Número máximo de conexões extras
+    )
+
     db.init_app(app)
     login_manager.init_app(app)
 
@@ -38,7 +47,8 @@ def create_app():
             license=project_info.get('license')
         )
 
+    # Registrar rotas dentro do contexto do aplicativo
     with app.app_context():
-        from . import routes  # Importe suas rotas
+        from . import routes  # Importe suas rotas aqui
 
     return app
